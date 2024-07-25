44762e6c03414c5d348ac37de750f5510a8255d9
Sandeep1696 <61002004+Sandeep1696@users.noreply.github.com>
2024-07-25 19:28:58 +0530

    basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base:"/Remix-UI/",
   plugins: [
     remix({
+      basename:"/Remix-UI/",
       ssr: false,
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
+      // future: {
+      //   v3_fetcherPersist: true,
+      //   v3_relativeSplatPath: true,
+      //   v3_throwAbortReason: true,
+      // },
     }),
     tsconfigPaths(),
   ],
